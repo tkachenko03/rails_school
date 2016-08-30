75cacb497bed99b942486280d2dc6736e734f1d2
Alexandr <a.w.tkachenko@gmail.com>
2016-08-30 19:40:20 +0300

    5.10end

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,17 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
-  def new
+def new
+  @article = Article.new
+end
+ 
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
   end
-
-	def create
-	  @article = Article.new(article_params)
-	 
-	  @article.save
-	  redirect_to @article
-	end
+end
  
 private
   def article_params
